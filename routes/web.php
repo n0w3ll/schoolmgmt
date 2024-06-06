1128665521b82f66795a4d6c7ab83769e4f7149f
n0w3ll <noel.dev2023@gmail.com>
2024-06-06 18:37:07 +0800

    add login function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'AuthLogin'])
+    ->name('login');
+Route::get('logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
 Route::get('admin/dashboard', function () {
     return view('admin.dashboard');
 });
